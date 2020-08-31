c25b4f08663123d743729233dc4abf57741b1832
huangshengxin <huangshengxin@xianlin.com>
2020-08-31 18:02:20 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
-class Person {
-  constructor(name) {
-    this.name = name
-  }
-  getName() {
-    return this.name
-  }
+// class Person {
+//   constructor(name,age) {
+//     this.name = name
+//     this.age = age
+
+//   }
+//   getName() {
+//     return this.name
+//   }
+//   eat(){
+//     console.log(`${this.name} eat something`)
+//   }
+//   speak(){
+//     console.log(`My name is ${this.name} ,age ${this.age}`)
+//   }
+// }
+// class Student extends Person {
+//   constructor(name,age,number) {
+//     super(name,age);
+//     this.number = number;
+//   }
+//   getName() {
+//     return this.name
+//   }
+//   eat(){
+//     console.log(`${this.name} eat something`)
+//     alert( `${this.name}在吃东西`);
+
+//   }
+//   speak(){
+//     console.log(`My name is ${this.name} ,age ${this.age}`)
+//   }
+// }
+// let person = new Person('hsx')
+// console.log(person.getName())
+// person.eat();
+function loadImg(src) {
+  return new Promise(function (resolve, reject) {
+    let img = document.createElement('img')
+    img.onload = function () {
+      resolve(img)
+    }
+    img.onerror = function () {
+      reject('图片加载失败')
+    }
+    img.src = src
+  })
 }
-let p = new Person('hsx')
-alert(p.getName())
-console(p.getName())
+let src = "https://dss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=1189779147,4248115718&fm=202&mola=new&crop=v1"
+let result = loadImg(src)
+result.then(function (img) {
+  alert(`width:${img.width}`)
+  return img
+}).then(img => {
+  alert(`height:${img.height}`)
+}).catch(err => {
+  alert(`报错：${err}`)
+})
