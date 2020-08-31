90581136b2a3a8cc19188b606884cc4cb81b6863
huangshengxin <hsx@zjxianlin.com>
2020-08-31 23:24:05 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -34,25 +34,36 @@
 // let person = new Person('hsx')
 // console.log(person.getName())
 // person.eat();
-function loadImg(src) {
-  return new Promise(function (resolve, reject) {
-    let img = document.createElement('img')
-    img.onload = function () {
-      resolve(img)
-    }
-    img.onerror = function () {
-      reject('图片加载失败')
-    }
-    img.src = src
-  })
+// function loadImg(src) {
+//   return new Promise(function (resolve, reject) {
+//     let img = document.createElement('img')
+//     img.onload = function () {
+//       resolve(img)
+//     }
+//     img.onerror = function () {
+//       reject('图片加载失败')
+//     }
+//     img.src = src
+//   })
+// }
+// let src = "https://dss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=1189779147,4248115718&fm=202&mola=new&crop=v1"
+// let result = loadImg(src)
+// result.then(function (img) {
+//   alert(`width:${img.width}`)
+//   return img
+// }).then(img => {
+//   alert(`height:${img.height}`)
+// }).catch(err => {
+//   alert(`报错：${err}`)
+// })
+class A {
+  constructor(time) {
+    this.time = time
+  }
+  work() {
+    return this.time
+  }
 }
-let src = "https://dss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=1189779147,4248115718&fm=202&mola=new&crop=v1"
-let result = loadImg(src)
-result.then(function (img) {
-  alert(`width:${img.width}`)
-  return img
-}).then(img => {
-  alert(`height:${img.height}`)
-}).catch(err => {
-  alert(`报错：${err}`)
-})
+function a(){
+  
+}
